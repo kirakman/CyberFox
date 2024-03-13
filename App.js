f26f97995d221df6837757861d8054fc6b867c06
kirakman <dourado178@gmail.com>
2024-03-13 17:29:07 -0300

    clear app.js and isolate home page

--- App.js
+++ App.js
@@ -1,60 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, ImageBackground, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet} from 'react-native';
 import Home from './screens/Home';
-import PrimaryButton from './components/PrimaryButton';
 
 export default function App() {
 
   return (
-    <ImageBackground 
-      source={require('./assets/VsnSy25aRTuBJx3-83GrKQ.png')} style={styles.backgroundImage}>
-
-      <View style={styles.logoContainer}>
-       <Home/>
-       <PrimaryButton/>
-       <View style={styles.textContainer}>
-          <Text style={styles.buttonText}>Já possui uma conta?</Text>
-          <TouchableOpacity>
-            <Text style={styles.titleText}>Login</Text>
-          </TouchableOpacity>
-      </View>    
-      </View>    
-
-    </ImageBackground>
+    <Home/>
   );
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    width: '100%', 
-    height: '100%', 
-    resizeMode: 'cover', 
-  },
-  logoContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    flex: 1,
-    alignContent: 'center',
-    alignItems: 'center'
-  },
-  textContainer: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    justifyContent: 'center',
-    paddingBottom: 25
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-  },
-  titleText: {
-    fontSize: 18,
-    color: 'white',
-    fontWeight: 'bold', 
-    marginLeft: 5,
-  },
+  
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,8 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground, TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import * as Font from 'expo-font';
+import PrimaryButton from '../components/PrimaryButton';
+
 
 const Home = () => {
 
@@ -21,6 +23,10 @@ const Home = () => {
     }
 
     return (
+        <ImageBackground 
+        source={require('../assets/VsnSy25aRTuBJx3-83GrKQ.png')} style={styles.backgroundImage}>
+  
+        <View style={styles.logoContainer}>
         <View style={styles.logoContainer}>
             <Image 
                 source={require('../assets/7j2QCS-QTOeQwWbbzVId3w-removebg-preview.png')} 
@@ -29,6 +35,17 @@ const Home = () => {
             <Text style={styles.primaryText}>CYBER</Text>
             <Text style={styles.secondaryText}>FOX</Text>
         </View>
+         <PrimaryButton/>
+         <View style={styles.textContainer}>
+            <Text style={styles.buttonText}>Já possui uma conta?</Text>
+            <TouchableOpacity>
+              <Text style={styles.titleText}>Login</Text>
+            </TouchableOpacity>
+        </View>    
+        </View>    
+  
+      </ImageBackground>
+        
     );
 }
 
@@ -64,7 +81,28 @@ const styles = StyleSheet.create({
         fontSize: 80,
         fontFamily: 'modern-rebel',
         color: '#CA7745',
-    }
+    },
+    button: {
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center'
+      },
+      textContainer: {
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        paddingBottom: 25
+      },
+      buttonText: {
+        fontSize: 18,
+        color: 'white',
+      },
+      titleText: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold', 
+        marginLeft: 5,
+      },
 });
 
 export default Home;
