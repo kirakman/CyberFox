edfbaf96e032850ceff0c83cb8bee23c63e39287
kirakman <dourado178@gmail.com>
2024-03-13 17:59:20 -0300

    Fix somethings

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { StyleSheet} from 'react-native';
-import Home from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home , Login, Register } from './screens/index';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
 
   return (
-    <Home/>
+    <NavigationContainer>
+    <Stack.Navigator
+    initialRouteName = 'Home'
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown:false
+        }}
+
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown:false
+        }}
+
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{
+          headerShown:false
+        }}
+
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+
   );
 }
 
