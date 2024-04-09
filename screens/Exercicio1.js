96568e1b7196c19d440756981420bb865fbd95ec
kirakman <dourado178@gmail.com>
2024-04-09 18:21:50 -0300

    item picker without logic

--- screens/Exercicio1.js
+++ screens/Exercicio1.js
@@ -1,11 +1,12 @@
 // Exercicio1.js
 
 import React, { useState } from 'react';
-import { View, Text, ImageBackground, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView, Image } from 'react-native';
 import Modal from "react-native-modal";
 import TituloExercicio from '../components/TituloExercicio';
 import TituloAula from '../components/TituloAula';
 import TituloQuiz from '../components/TituloQuiz';
+import { Picker } from '@react-native-picker/picker';
 
 const Exercicio1 = () => {
     const [isModalVisible1, setModalVisible1] = useState(true); 
@@ -47,6 +48,8 @@ const Exercicio1 = () => {
         setModalVisible5(!isModalVisible5);
     };
 
+    const [selectedOption1, setSelectedOption1] = useState(null);
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
@@ -56,19 +59,19 @@ const Exercicio1 = () => {
                         <Modal isVisible={isModalVisible1}>
                             <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white', borderColor: 'white', borderRadius: 15}}>
                                 <ScrollView style={{ width: '90%' }}>
-                                    <TituloAula nomeAula="O que é Pishing?"/>
+                                    <TituloAula nomeAula="O que é Phishing?"/>
                                     <Text style={styles.paragraph}>
                                         {'\u2022'} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius eros eget enim aliquet, ac hendrerit orci volutpat. Sed convallis velit nec libero vestibulum, sit amet feugiat lacus vulputate. Fusce a magna eros. Proin nec consectetur sapien. Duis vel diam eget ex convallis vulputate. Cras congue mi eu libero ultricies, ut cursus odio vehicula. Integer condimentum eleifend malesuada. Sed placerat mi a odio malesuada, et convallis velit bibendum. Duis nec ligula in odio ultrices fringilla. Curabitur ac felis nec nulla posuere consequat. Pellentesque in arcu nunc.
                                     </Text>
-                                    <TituloAula nomeAula="Onde é aplicado o Pishing?"/>
+                                    <TituloAula nomeAula="Onde é aplicado o Phishing?"/>
                                     <Text style={styles.paragraph}>
                                         {'\u2022'} Morbi non est in justo dapibus tempor. Mauris vel tortor sed leo accumsan convallis vel a elit. Aliquam id magna libero. Vestibulum finibus ligula a felis lacinia, ut accumsan leo vehicula. Nullam finibus tempus erat, sit amet mattis ex blandit id. Donec non tempor libero. Pellentesque non magna vitae ligula dapibus lacinia. Nullam ac eros odio. Maecenas laoreet odio quis eleifend efficitur. Phasellus vitae est a nisi malesuada blandit. Nulla facilisi.
                                     </Text>
-                                    <TituloAula nomeAula="Como identificar o Pishing via E-mail?"/>
+                                    <TituloAula nomeAula="Como identificar o Phishing via E-mail?"/>
                                     <Text style={styles.paragraph}>
                                         {'\u2022'} Praesent id odio vel felis commodo eleifend nec eget lorem. Nam fermentum purus sed vehicula volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam suscipit velit ut justo fermentum, ac vestibulum neque sodales. Ut commodo arcu id posuere convallis. Proin vel elit faucibus, pharetra lorem sit amet, placerat libero. Aenean ut accumsan leo, non pharetra magna. Nam rhoncus rhoncus vestibulum. Nulla sit amet tortor nisi. Sed consectetur, ante sed ultrices fringilla, quam odio varius lorem, sed luctus orci magna nec nisi. Etiam rutrum enim nec orci fermentum, id bibendum lacus pharetra. Sed ac efficitur velit, sit amet fringilla justo.
                                     </Text>
-                                    <TituloAula nomeAula="Evitando o Pishing..."/>
+                                    <TituloAula nomeAula="Evitando o Phishing..."/>
                                     <Text style={styles.paragraph}>
                                         {'\u2022'} Duis at congue felis. Praesent sagittis tristique nulla nec lacinia. Vivamus suscipit auctor massa, id fringilla orci posuere in. Fusce vulputate mi ut diam pharetra, id bibendum ex dictum. Curabitur sed nisl sit amet nunc tempus tempor a ac neque. Integer at leo sed magna faucibus commodo. Curabitur euismod enim eget ex pretium, et dictum ipsum aliquet. Phasellus nec vestibulum elit. Nulla luctus vehicula augue, ac tempor lorem pharetra a. Vivamus dictum, dolor vel ullamcorper volutpat, odio ligula auctor velit, nec gravida ipsum quam ut libero. Vivamus condimentum nunc nec mauris fringilla, in volutpat metus tristique. Sed ullamcorper, mauris a varius mattis, urna purus laoreet dolor, eu laoreet elit ante at sem. Donec venenatis, lorem nec interdum tincidunt, metus arcu sollicitudin libero, et bibendum eros libero a justo. In hac habitasse platea dictumst. Ut efficitur scelerisque magna, nec dictum sapien consequat in. Sed hendrerit malesuada turpis, at tincidunt nisl elementum a. Sed sed eros nec libero placerat convallis.
                                     </Text>
@@ -82,10 +85,30 @@ const Exercicio1 = () => {
 
                             {/* Primeira pergunta */}
                             <View>
-                            <TituloQuiz nomeQuiz="O que é Pishing?" onPress={toggleModal2}/>
+                            <TituloQuiz nomeQuiz="O que é Phishing?" onPress={toggleModal2}/>
                             <Modal isVisible={isModalVisible2}>
                             <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white', borderColor: 'white', borderRadius: 15}}>
-                            <TituloAula nomeAula="O que é Pishing?"/>
+                            <TituloAula nomeAula="O que é Phishing?"/>
+                            <Image source={require('../assets/foxCA7745.png')}
+                                    style={{resizeMode: 'contain', height: 200}}
+                            />
+                        
+                            <View style={styles.definicoes}>
+                                <Text style={{fontSize: 18}}>A: Um ataque que tenta roubar informações pessoais, como números de cartão de crédito e senhas, fingindo ser legítimo.</Text>
+                                <Text style={{fontSize: 18}}>B: Uma técnica para cultivar algas marinhas.</Text>
+                                <Text style={{fontSize: 18}}>C: Desenvolver habilidades de pescaria recreativa em áreas marinhas protegidas.</Text>
+                                <Text style={{fontSize: 18}}>D: Um jogo de RPG.</Text>
+                            </View>
+
+                            <Picker
+                                selectedValue={selectedOption1}
+                                onValueChange={(itemValue, itemIndex) => setSelectedOption1(itemValue)} style={styles.pickerStyles}>
+                                <Picker.Item label="Opção A" value="option1" />
+                                <Picker.Item label="Opção B" value="option2" />
+                                <Picker.Item label="Opção C" value="option3" />
+                                <Picker.Item label="Opção D" value="option4" />
+                            </Picker>
+
                             <TouchableOpacity onPress={closeModal2} style={styles.closeButton}>
                                 <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>Voltar</Text>
                             </TouchableOpacity>
@@ -95,10 +118,10 @@ const Exercicio1 = () => {
 
                             {/* Segunda pergunta */}
                             <View>
-                            <TituloQuiz nomeQuiz="Onde é aplicado o Pishing?" onPress={toggleModal3}/>
+                            <TituloQuiz nomeQuiz="Onde é aplicado o Phishing?" onPress={toggleModal3}/>
                             <Modal isVisible={isModalVisible3}>
                             <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white', borderColor: 'white', borderRadius: 15}}>
-                            <TituloAula nomeAula="Onde é aplicado o Pishing?"/>
+                            <TituloAula nomeAula="Onde é aplicado o Phishing?"/>
                             <TouchableOpacity onPress={closeModal3} style={styles.closeButton}>
                                 <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>Voltar</Text>
                             </TouchableOpacity>
@@ -108,10 +131,10 @@ const Exercicio1 = () => {
 
                             {/* Terceira pergunta */}
                             <View>
-                            <TituloQuiz nomeQuiz="Como identificar o Pishing via E-mail?" onPress={toggleModal4}/>
+                            <TituloQuiz nomeQuiz="Como identificar o Phishing via E-mail?" onPress={toggleModal4}/>
                             <Modal isVisible={isModalVisible4}>
                             <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white', borderColor: 'white', borderRadius: 15}}>
-                            <TituloAula nomeAula="Como identificar o Pishing via E-mail?"/>
+                            <TituloAula nomeAula="Como identificar o Phishing via E-mail?"/>
                             <TouchableOpacity onPress={closeModal4} style={styles.closeButton}>
                                 <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>Voltar</Text>
                             </TouchableOpacity>
@@ -121,10 +144,10 @@ const Exercicio1 = () => {
 
                             {/* Quarta pergunta */}
                             <View>
-                            <TituloQuiz nomeQuiz="Evitando o Pishing..." onPress={toggleModal5}/>
+                            <TituloQuiz nomeQuiz="Evitando o Phishing..." onPress={toggleModal5}/>
                             <Modal isVisible={isModalVisible5}>
                             <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'white', borderColor: 'white', borderRadius: 15}}>
-                            <TituloAula nomeAula="Evitando o Pishing..."/>
+                            <TituloAula nomeAula="Evitando o Phishing..."/>
                             <TouchableOpacity onPress={closeModal5} style={styles.closeButton}>
                                 <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>Voltar</Text>
                             </TouchableOpacity>
@@ -163,7 +186,18 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         backgroundColor: '#CA7745',
         marginTop: 20
-    }
+    },
+    pickerStyles: {
+        height: 50,
+        width: 200,
+        marginTop: 20,
+        marginBottom: 20,
+      },
+      definicoes: {
+        gap: 16,
+        marginTop: 25,
+        paddingBottom: 25
+      }
 });
 
 export default Exercicio1;
